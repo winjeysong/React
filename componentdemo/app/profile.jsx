07da6a3114a28043e9331b7d2d244067b7679b55
winjeysong <winjey-song@163.com>
2017-08-07 00:44:49 +0800

    Add new code

--- componentdemo/app/profile.jsx
+++ componentdemo/app/profile.jsx
@@ -6,12 +6,30 @@ const propTypes = {  //属性验证
 };
 
 class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  //添加点赞的状态
+            liked: 0
+        };
+        this.likedCallback = this.likedCallback.bind(this);
+    }
+
+    likedCallback() {  //给onClick单击事件添加的回调函数
+        let liked = this.state.liked;
+        liked += 1;
+        this.setState({
+            liked
+        });
+    }
+
     render() {
         return (
             <div className="profile-component">
                 {/*属性访问*/}
                 <h1>My name is {this.props.name}.</h1>
                 <h2>I&#39;m {this.props.age} years old.</h2>
+                <button onClick={this.likedCallback}>Like Me</button>
+                <h2>total amount of likes：{this.state.liked}</h2>
             </div>
         );
     }
