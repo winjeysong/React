1b31fae8ca8ca8715f705811e6b955e84a3a84b5
winjeysong <winjey-song@163.com>
2017-08-08 19:34:10 +0800

    Add hobby.jsx & some changes

--- componentdemo/app/profile.jsx
+++ componentdemo/app/profile.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from "react";
+import Hobby from "./hobby";
 
 const propTypes = {  //属性验证
     name: PropTypes.string.isRequired,
@@ -9,7 +10,8 @@ class Profile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {  //添加点赞的状态
-            liked: 0
+            liked: 0,
+            hobbies: ["mystic movie", "electronic music", "photographing"]
         };
         this.likedCallback = this.likedCallback.bind(this);
     }
@@ -29,7 +31,12 @@ class Profile extends React.Component {
                 <h1>My name is {this.props.name}.</h1>
                 <h2>I&#39;m {this.props.age} years old.</h2>
                 <button onClick={this.likedCallback}>Like Me</button>
-                <h2>total amount of likes：{this.state.liked}</h2>
+                <h2>total amount of likes: {this.state.liked}</h2>
+                <h2>My hobbies:</h2>
+                <ul>
+                    {this.state.hobbies.map((hobby, i) => <Hobby index={i} hobby={hobby} />)}
+                    {/*遍历hobbies并将值传入到Hobby组件中，注意给每个循环组件添加一个唯一的index值*/}
+                </ul>
             </div>
         );
     }
