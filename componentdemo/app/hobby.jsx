fcb760cd0fca322d7773ad66a8f196edb95c99d5
winjeysong <winjey-song@163.com>
2017-08-13 19:51:08 +0800

    Stateless functional component

--- componentdemo/app/hobby.jsx
+++ componentdemo/app/hobby.jsx
@@ -3,11 +3,14 @@ import React, { PropTypes } from "react";
 const propTypes = {
     hobby: PropTypes.string.isRequired
 };
-
+/*
 class Hobby extends React.Component {
     render() {
         return <li>{this.props.hobby}</li>;
     }
+}*/
+function Hobby(props) {
+    return <li>{props.hobby}</li>;
 }
 
 Hobby.propTypes = propTypes;
