f780c546a0d6d473f63f7d7f16aaadf89d1993ac
winjeysong <winjey-song@163.com>
2017-08-17 20:41:42 +0800

    Finish the itemeditor

--- proj_notemark/components/ItemEditor/ItemEditor.jsx
+++ proj_notemark/components/ItemEditor/ItemEditor.jsx
@@ -7,26 +7,56 @@ import React, { PropTypes } from "react";
 
 //prop validation
 const propTypes = {
-    item: PropTypes.object.isRequired,
-
+    item: PropTypes.object,
+    onSave: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired
 }
 
 //stateless function
 function ItemEditor() {
+    const { onSave, onCancel } = this.props;
+    const item = this.props.item || {
+        title: "",
+        content: ""
+    };
+    let editText = item.id ? "保存" : "创建";
+    let save = () => {
+        onSave({
+            ...item,
+            title: this.titleInput,
+            content: this.contentInput
+        });
+    };
+
     return (
         <div className="">
             <div className="control-btn">
-                <button className="">发布</button>
+                <button onClick={save} className="">{editText}</button>
                 <button className="">取消</button>
             </div>
-            <input type="text" className="" value="请输入标题"/>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <div className="edit-box">
+                <input
+                    ref={
+                        (input) => { this.titleInput = input; }
+                    }
+                    defaultValue={item.title}
+                    type="text" className="" placeholder="请输入标题"
+                />
+                <textarea
+                    ref={
+                        (textarea) => { this.contentInput = textarea; }
+                    }
+                    defaultValue={item.content}
+                    name="" id="" placeholder="开始写作吧"
+                >
+                </textarea>
+            </div>
         </div>
     );
 }
 
 //add prop validation
-ListItem.propTypes = propTypes;
+ItemEditor.propTypes = propTypes;
 
 //export this component
-export default ListItem;
+export default ItemEditor;
