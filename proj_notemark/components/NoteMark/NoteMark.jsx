ccc7619cac5b80a2a44e2566ac4efc29495fe1f8
winjeysong <winjey-song@163.com>
2017-09-16 23:56:19 +0800

    Re-add notemark
    Miss notemark for forgetting to commit it

---
+++ proj_notemark/components/NoteMark/NoteMark.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import uuid from "uuid";
+
+import CreateBar from "../CreateBar/CreateBar";
+import List from "../List/List";
+import ItemEditor from "../ItemEditor/ItemEditor";
+import ItemShowLayer from "../ItemShowLayer/ItemShowLayer";
+
+
+export default class NoteMark extends React.Component {
+    
+}
