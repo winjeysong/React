b69b1591330b4726b83ebf02af1830ca672932fd
winjeysong <winjey-song@163.com>
2017-09-19 23:58:47 +0800

    Edit listitem

--- proj_notemark/components/ListItem/ListItem.jsx
+++ proj_notemark/components/ListItem/ListItem.jsx
@@ -4,6 +4,7 @@
 
 //load dependencies
 import React, { PropTypes } from "react";
+import "../../styles/ListItem.scss";
 
 //prop validation
 const propTypes = {
@@ -17,19 +18,23 @@ function ListItem({ item, onClick }) {
     if (item.time) {
         myTime = new Date(item.time).toISOString().match(/(\d{4}-\d{2}-\d{2})/)[0];  //获取日期，类似于1970-01-01这样的格式
     }
-
+    let titleString = item.title.toString();
+    let myTitle = titleString.length <= 8 ? item.title : `${titleString.slice(0, 7)}...`;
     return (
-        <button
-            className="list-item-btn"
-            onClick={onClick}
-        >
+        <div className="list-item">
             <span className="list-item-time">
                 {myTime}
             </span>
             <span className="list-item-title">
-                {item.title}
+                {myTitle}
             </span>
-        </button>
+            <button
+                className="list-item-btn btn btn-default"
+                onClick={onClick}
+            >
+                阅读
+            </button>
+        </div>
     );
 }
 
