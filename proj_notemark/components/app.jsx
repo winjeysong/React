f95d0fd015adcb774adc1d8ad7ef240f8df3f4fd
winjeysong <winjey-song@163.com>
2017-09-20 02:43:14 +0800

    import new package

--- proj_notemark/components/app.jsx
+++ proj_notemark/components/app.jsx
@@ -1,6 +1,9 @@
+import "babel-polyfill";
 import React from "react";
 import { render } from "react-dom";
 import NoteMark from "./NoteMark/NoteMark";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const app = document.createElement("div");
 document.body.appendChild(app);
