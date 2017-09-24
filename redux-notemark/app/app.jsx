37dfbf5c92ad13666bda357be3c6c5a1c87c3abc
winjeysong <winjey-song@163.com>
2017-09-24 21:58:12 +0800

    Add root app component

--- redux-notemark/app/app.jsx
+++ redux-notemark/app/app.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import ToDo from  "./component/ToDo";
+import { render } from "react-dom";
+import { bindActionCreators, createStore, applyMiddleware } from "redux";
+import { connect, Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
 
-const APP = document.createElement("div");
-document.body.appendChild(APP);
-ReactDOM.render(<ToDo />, APP);
+import NoteMark from "./components/NoteMark";
+
+import combinedReducer from "./reducers";
+import * as actionCreators from "./actions";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+//create store with middleware
+const store = applyMiddleware(
+    thunkMiddleware
+)(createStore)(combinedReducer);
+
+//root component
+const App = connect(
+    state => ({ state }),
+    dispatch => ({
+        actions: bindActionCreators(actionCreators, dispatch)
+    })
+)(NoteMark);
+
+// create DOM container
+const container = document.body.appendChild(
+    document.createElement("div")
+);
+
+// render root conponent with store to DOM container
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    container
+);
+
+/*render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
+*/
