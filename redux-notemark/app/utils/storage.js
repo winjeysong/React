f1b885d2c5643179f66f02f5411a90e826e9e651
winjeysong <winjey-song@163.com>
2017-09-24 15:47:28 +0800

    Add storage

---
+++ redux-notemark/app/utils/storage.js
@@ -0,0 +1,75 @@
+/**
+ * @ local storage
+ */
+
+import uuid from "uuid";
+
+const STORAGE = window.localStorage;
+const STORAGE_KEY = "notemark";
+
+export function getAll() {
+    return new Promise((resolve) => {
+        const results = STORAGE.getItem(STORAGE_KEY);
+
+        try {
+            resolve(
+                results ? JSON.parse(results) : []
+            );
+        } catch (e) {
+            resolve([]);
+        }
+    });
+}
+
+export function saveAll(results) {
+    return new Promise((resolve) => {
+        STORAGE.setItem(
+            STORAGE_KEY,
+            JSON.stringify(results)
+        );
+
+        resolve();
+    });
+}
+
+export function getEntry(id) {
+    return getAll().then(
+        results => results.find(
+            result => result.id === id
+        )
+    );
+}
+
+export function insertEntry(title, content) {
+    const entry = {
+        title,
+        content,
+        id: uuid.v4(),
+        time: new Date().getTime()
+    };
+
+    return getAll()
+        .then(results => [...results, entry])
+        .then(saveAll)
+        .then(() => entry);
+}
+
+export function updateEntry(id, title, content) {
+    let entry;
+    return getAll()
+        .then(
+            results => results.map(
+                result => (
+                    result.id === id
+                        ? (entry === {
+                            ...result,
+                            title,
+                            content
+                        })
+                        : result
+                )
+            )
+        )
+        .then(saveAll)
+        .then(() => entry);
+}
