c3cd79a357040e51f2a72303946479aa2a62203a
winjeysong <winjey-song@163.com>
2017-09-24 15:47:16 +0800

    Add action

---
+++ redux-notemark/app/actions/index.js
@@ -0,0 +1,81 @@
+/**
+ * @ actions
+ */
+
+import * as storage from "../utils/storage";
+
+export const SELECT_ENTRY = "SELECT_ENTRY";
+export const CREATE_NEW_ENTRY = "CREATE_NEW_ENTRY";
+export const EDIT_ENTRY = "EDIT_ENTRY";
+export const CANCEL_EDIT = "CANCEL_EDIT";
+
+export function selectEntry(id) {
+    return {
+        type: SELECT_ENTRY,
+        id
+    };
+}
+
+export function createNewEntry() {
+    return {
+        type: CREATE_NEW_ENTRY
+    };
+}
+
+export function editEntry(id) {
+    return {
+        type: EDIT_ENTRY,
+        id
+    };
+}
+
+export function cancelEdit() {
+    return {
+        type: CANCEL_EDIT
+    };
+}
+
+export const UPDATE_ENTRY_LIST = "UPDATE_ENTRY_LIST";
+
+function updateEntryList(items) {
+    return {
+        type: UPDATE_ENTRY_LIST,
+        items
+    };
+}
+
+export function deleteEntry(id) {
+    return (dispatch) => {
+        storage.deleteEntry(id)
+            .then(() => storage.getAll())
+            .then(items => dispatch(updateEntryList(items)));
+    };
+}
+
+export function fetchEntryList() {
+    return (dispatch) => {
+        storage.getAll()
+            .then(items => dispatch(updateEntryList(items)));
+    };
+}
+
+export const UPDATE_SAVED_ENTRY = "UPDATE_SAVED_ENTRY";
+
+export function updateSavedEntry(item) {
+    const { title, content, id } = item;
+    return (dispatch) => {
+        //update
+        if (id) {
+            storage.updateEntry(id, title, content)
+                .then(() => dispatch(updateSavedEntry(id)))
+                .then(() => storage.getAll())
+                .then(items => dispatch(updateEntryList(items)));
+        } else {
+        //create
+            storage.insertEntry(title, content)
+                .then(insert => dispatch(updateSavedEntry(insert.id)))
+                .then(() => storage.getAll())
+                .then(items => dispatch(updateEntryList(items)));
+        }
+    };
+}
