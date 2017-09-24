08b90c14812785e712f9c4be9d438d58f1d31036
winjeysong <winjey-song@163.com>
2017-09-24 21:56:23 +0800

    Add itemeditor

--- redux-notemark/app/components/ItemEditor/index.jsx
+++ redux-notemark/app/components/ItemEditor/index.jsx
@@ -4,6 +4,7 @@
 
 //load dependencies
 import React, { PropTypes } from "react";
+import "./style.scss";
 
 //prop validation
 const propTypes = {
@@ -12,53 +13,63 @@ const propTypes = {
     onCancel: PropTypes.func.isRequired
 };
 
-//stateless function
-class ItemEditor extends React.Component {
-    render() {
-        let titleInput = null;
-        let contentInput = null;
-        const { onSave, onCancel } = this.props;
-        const item = this.props.item || {
-            title: "",
-            content: ""
-        };
-        const editText = item.id ? "保存" : "创建";
-        const save = () => {
-            onSave({
-                item,
-                title: titleInput.value,
-                content: contentInput.value
-            });
-        };
-        return (
-            <div className="item-editor">
-                <div className="control-btn">
-                    <button onClick={save} className="btn create-save-btn">{editText}</button>
-                    <button onClick={onCancel} className="btn cancel-btn">取消</button>
-                </div>
-                <div className="edit-box">
+function ItemEditor({ item, onSave, onCancel }) {
+    let titleInput = null;
+    let contentInput = null;
+    let saveText = item.id ? "保存" : "创建";
+    let save = () => {
+        onSave({
+            ...item,
+            title: titleInput.value,
+            content: contentInput.value
+        });
+    };
+    return (
+        <div className="item-editor">
+            <div className="control-btn">
+                <button
+                    onClick={save}
+                    className="btn create-save-btn btn-success"
+                >
+                    {saveText}
+                </button>
+                <button
+                    onClick={onCancel}
+                    className="btn cancel-btn btn-warning"
+                >
+                    取消
+                </button>
+            </div>
+            <div className="edit-box">
+                <div className="input-group title-input">
+                    <span className="input-group-addon" id="addon1">
+                        标题
+                    </span>
                     <input
-                        ref={
-                            (input) => { titleInput = input; }
-                        }
-                        defaultValue={item.title}
+                        ref={(input) => { titleInput = input; }}
                         type="text"
-                        className=""
+                        defaultValue={item.title}
+                        className="form-control"
                         placeholder="请输入标题"
+                        aria-describedby="addon1"
                     />
+                </div>
+                <div className="input-group">
+                    <span className="input-group-addon" id="addon2">
+                        正文
+                    </span>
                     <textarea
-                        ref={
-                            (textarea) => { contentInput = textarea; }
-                        }
+                        ref={(input) => { contentInput = input; }}
                         defaultValue={item.content}
-                        name=""
-                        id=""
+                        className="form-control"
+                        aria-describedby="addon2"
                         placeholder="开始写作吧"
+                        rows="35"
                     />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 //add prop validation
